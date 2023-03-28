e373d4e6a170193ce8305722e1c1a2a645a09295
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2023-03-28 23:49:26 +0200

    Zle haslo

--- springboot-backend/src/main/resources/db/migration/V1__createUserTable.sql
+++ springboot-backend/src/main/resources/db/migration/V1__createUserTable.sql
@@ -14,5 +14,5 @@ CREATE TABLE IF NOT EXISTS user
     );
 
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`, `verification_code`, `enabled`, `reset_token`)
-VALUES (NULL, 'Marek', 'Nowak', 'mnowak@example.com', 'Test12345', 'ROLE_ADMIN', NULL, 1, NULL),
-       (NULL, 'Adam', 'Kowalski', 'akowalski@example.com', 'Test12345', 'ROLE_USER', NULL, 1, NULL);
+VALUES (NULL, 'Marek', 'Nowak', 'mnowak@example.com', '$2a$12$0bmGc4vmz2R/zP9kJuQF1utMU94kXR8kY3nKqbTwlB5rvA2Sh92Wq', 'ROLE_ADMIN', NULL, 1, NULL),
+       (NULL, 'Adam', 'Kowalski', 'akowalski@example.com', '$2a$12$0bmGc4vmz2R/zP9kJuQF1utMU94kXR8kY3nKqbTwlB5rvA2Sh92Wq', 'ROLE_USER', NULL, 1, NULL);
